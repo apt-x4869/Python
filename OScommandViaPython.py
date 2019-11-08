7c23f28eb8f9e30d7b9d55aa08942a1b1b66c59f
aman.pasi@gmail.com <aman.pasi@gmail.com>
2019-11-08 22:21:29 +0530

    More method Added

--- OScommandViaPython.py
+++ OScommandViaPython.py
@@ -1,8 +1,12 @@
 #Running Os command via Python
 import os
+import subprocess
 
 if __name__ == "__main__" :
-    os.popen(
-        'start notepad++ YourFile.txt'
-        )
-    os.system("notepad;cmd.exe") #Running cmd before notepad wont run notepad
+    rd = os.popen("git --version").read()
+    print(rd)
+    os.system("npm --version")
+    os.system("node --version")
+    returned_output = subprocess.check_output("date")
+    print(returned_output)
+    subprocess.call("date")
